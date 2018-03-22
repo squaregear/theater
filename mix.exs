a6c2f1fb24ab12e4a7d63d497565e8a229b076af
mwelch <MatthewWelch@QuickenLoans.com>
2018-03-22 07:52:46 -0400

    exdoc in dev?

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Theater.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:ex_doc, "~> 0.18", only: :docs},
+      {:ex_doc, "~> 0.18", only: :dev},
     ]
   end
 
